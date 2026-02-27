16f4f457e4ad22c7473a5c1d5e3cd91a702d5812
asyrafabdzawawi <66741161+asyrafabdzawawi@users.noreply.github.com>
2026-02-27 09:48:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,5 +58,8 @@ def generate_pdf():
 
     return send_file(filepath, as_attachment=True)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
